6d1e74d48a7ffffdc224993d7576290c6abfcc87
Žygimantas <zyg.sid2@gmail.com>
2019-05-30 10:41:02 +0300

    Fixed Ulam_spiral outout

--- Ulam_spiral/main.cpp
+++ Ulam_spiral/main.cpp
@@ -6,6 +6,26 @@
 
 using namespace std;
 
+bool isPrime(int n){
+    int i;
+    bool isPrime = true;
+
+    for(i = 2; i <= n / 2; ++i)
+    {
+        if(n % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+        return true;
+    else
+        return false;
+
+    return 0;
+}
+
 int main()
 {
     int n;
@@ -22,19 +42,25 @@ int main()
         for(int c=1;c<i+1;c++)
         {
             int z=tl[c-1];
+            if(isPrime(z+i-c+1))
             cout<<setw(3)<<z+i-c+1;
+            else cout << setw(3) << "*";
         }
         cout<<setw(3)<<tl[i];
         int p=tl[i]-1;
         for(int j=0;j<n-2*i-1;j++)
         {
+            if(isPrime(p))
             cout<<setw(3)<<p;
+            else cout << setw(3) << "*";
             p--;
         }
         k-=2;
         for(int m=0;m<i;m++)
         {
+            if(isPrime(tr[n/2-i+m]))
             cout<<setw(3)<<tr[n/2-i+m];
+            else cout << setw(3) << "*";
             tr[n/2-i+m]--;
         }
         cout<<endl;
@@ -51,18 +77,24 @@ int main()
         int x=n/2-1-i;
         for(int j=0;j<n/2-i;j++)
         {
+            if(isPrime(bl[j]-x))
             cout<<setw(3)<<bl[j]-x;
+            else cout << setw(3) << "*";
             x--;
         }
         for(int a=0;a<2*(i+1);a++)
         {
             bl[n/2-i-1]++;
+            if(isPrime(bl[n/2-i-1]))
             cout<<setw(3)<<bl[n/2-i-1];
+            else cout << setw(3) << "*";
         }
         for(int c=0;c<n/2-i-1;c++)
         {
             int h=br[c+i]+c+1;
+            if(isPrime(h))
             cout<<setw(3)<<h;
+            else cout << setw(3) << "*";
         }
         cout<<endl;
     }
